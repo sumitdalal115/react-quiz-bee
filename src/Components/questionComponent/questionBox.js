5d08355f46a1c706276cfb110bb2a28f0942dc9a
sumit dalal <sumit.dalal115@gmail.com>
2020-03-13 07:22:45 +0530

    other way of fixing same issue

--- src/Components/questionComponent/questionBox.js
+++ src/Components/questionComponent/questionBox.js
@@ -7,11 +7,13 @@ class QuestionBox extends Component {
     }
 
     setAnswer = (answer) => {
-        console.log('im setting answer')
-        this.setState({
-            options: [answer],
-            answer: answer
-        });
+        if(this.state.answer === '') {
+            this.setState({
+                options: [answer],
+                answer: answer
+            });
+            this.props.onSelection(answer);
+        }
     }
 
     render(){
@@ -19,16 +21,8 @@ class QuestionBox extends Component {
             <div className="questionBox">
                 <div className="question">{this.props.question}</div>
                 {
-                    this.state.answer === '' && this.state.options.map((option, index) => (
-                        <button key={index} className="answerBtn" onClick={ () => { this.setAnswer(option); this.props.onSelection(option) }}> 
-                            {option} 
-                        </button> 
-                    ))
-                    
-                }
-                {
-                    this.state.answer !== '' && this.state.options.map((option, index) => (
-                        <button key={index} className="answerBtn"> 
+                    this.state.options.map((option, index) => (
+                        <button key={index} className="answerBtn" onClick={ () => { this.setAnswer(option)} }> 
                             {option} 
                         </button> 
                     ))
