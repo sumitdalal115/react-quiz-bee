8c1a0fa0997574596a1550e74f38e7d0b3cf5952
sumit dalal <sumit.dalal115@gmail.com>
2020-04-02 00:16:08 +0530

    mimic tab behaviour - introduced test view

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ import QuestionBox from "./Components/questionComponent/questionBox";
 import ResultBox from "./Components/resultComponent/result"
 
 class QuizBee extends Component {
+
+    view = {
+        QUIZ: 'quiz',
+        STATUS: 'status',
+        USER: 'user',
+        TEST: 'test'
+    }
+
     state = {
         questionBank: [],
         score: 0,
-        attemptCount : 0
-    }
+        attemptCount : 0,
+        currentView : this.view.QUIZ
+    }    
 
     getQuestions = () => {
         console.log('QuizBee: fetching questions');
@@ -43,6 +52,13 @@ class QuizBee extends Component {
         this.getQuestions();
     }
 
+    loadTestView = () => {
+        var newView = this.state.currentView === this.view.TEST ? this.view.QUIZ : this.view.TEST;
+        this.setState({
+            currentView:newView
+        }) 
+    }
+
     componentDidMount() {
         console.log('QuizBee: componentDidMount*******');
         this.getQuestions();
@@ -56,11 +72,14 @@ class QuizBee extends Component {
         return (
         <div className="container">
             <div className="title">QuizBee 
+                <button className="resetBtn" onClick={this.loadTestView}>Toggle Test view</button>
                 <button className="resetBtn" onClick={this.resetGame}>Reset</button>
             </div>
-            { this.state.questionBank.length > 0 
-            && this.state.attemptCount < 5 
-            && this.state.questionBank.map(
+            {
+                this.state.currentView === this.view.QUIZ
+                && this.state.questionBank.length > 0 
+                && this.state.attemptCount < 5 
+                && this.state.questionBank.map(
                 ({question, answers, correct, questionId}) => 
                     <QuestionBox question={question} 
                         options={answers} 
@@ -69,7 +88,15 @@ class QuizBee extends Component {
                         onSelection= {answer => this.computeAnswer(answer, correct)} /> 
             )}
             {
-                this.state.attemptCount === 5 && <ResultBox score={this.state.score} reset={this.resetGame} />
+                this.state.currentView === this.view.QUIZ 
+                && this.state.attemptCount === 5 
+                && <ResultBox score={this.state.score} reset={this.resetGame} />
+            }
+            {
+               this.state.currentView === this.view.TEST 
+               && <div className="score-board">            
+                    <div className="score">hi, this is test view!!</div>
+                </div>
             }
         </div>
         );
